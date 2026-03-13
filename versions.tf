da3960c0af6692945ab29b8d7c14ee0c4c6fc8c7
Vishal Panwar <vishalpanwar@Vishals-MacBook-Air.local>
2026-03-14 05:16:09 +0530

    Configure Terraform Cloud backend

--- versions.tf
+++ versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  cloud {
+    organization = "DiscovrAI"
+    workspaces {
+      name = "infra"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
